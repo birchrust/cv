eebb5fa3b240880f85b310764c4cd20e715533a6
birchrust <pickboa@gmail.com>
2023-08-11 17:42:27 +0800

    feat: update container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "xl": "1080px",
+      },
+    },
     extend: {},
   },
   plugins: [],
